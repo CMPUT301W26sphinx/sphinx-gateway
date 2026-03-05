5bbb1e07646e661a13f62a78556d21f5d7de7c21
NotDeoxide <bryanjonathan87293@gmail.com>
2026-03-05 02:43:24 -0700

    basic dependancies

--- app/app/build.gradle.kts
+++ app/app/build.gradle.kts
@@ -48,5 +48,17 @@ dependencies {
     // Add the dependency for the Firebase Authentication library
     // When using the BoM, you don't specify versions in Firebase library dependencies
     implementation("com.google.firebase:firebase-auth")
+<<<<<<< HEAD
     implementation("com.google.firebase:firebase-firestore")
+=======
+
+    // QR Code dependancies - google based dependancies
+    implementation("com.google.mlkit:barcode-scanning:17.3.0")
+
+    // Camera dependencies
+    implementation("androidx.camera:camera-core:1.3.4")
+    implementation("androidx.camera:camera-camera2:1.3.4")
+    implementation("androidx.camera:camera-lifecycle:1.3.4")
+    implementation("androidx.camera:camera-view:1.3.4")
+>>>>>>> e74ffb1 (basic dependancies)
 }
