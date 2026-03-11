5390031c74e30ae9b3e15f6cb59dc0547e329bca
nzapisocki <nzapisoc@ualberta.ca>
2026-03-11 11:43:00 -0600

    Implemented ProfileEdit Fragment to update info
    ProfileEdit fragment updates information  displayed to match current information saved in user profile.

--- app/app/src/main/java/com/example/eventlotterysystem/UI/fragments/ProfileEditFragment.java
+++ app/app/src/main/java/com/example/eventlotterysystem/UI/fragments/ProfileEditFragment.java
@@ -4,12 +4,16 @@ import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.EditText;
+import android.widget.TextView;
+import android.widget.Toast;
 
 import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
 import androidx.fragment.app.Fragment;
 
 import com.example.eventlotterysystem.R;
+import com.example.eventlotterysystem.database.ProfileManager;
 
 public class ProfileEditFragment extends Fragment {
     @Nullable
@@ -19,5 +23,42 @@ public class ProfileEditFragment extends Fragment {
 
     }
 
+    @Override
+    public void onViewCreated(@NonNull View view, @Nullable Bundle savedInstanceState) {
+        super.onViewCreated(view, savedInstanceState);
+        // display the current user information
+        // update views
+        TextView nameTextView = view.findViewById(R.id.edit_first_name);
+        TextView emailTextView = view.findViewById(R.id.edit_email);
+        TextView phoneTextView = view.findViewById(R.id.edit_phone_number);
 
+        ProfileManager manager = new ProfileManager();
+
+        manager.getUserProfile(user -> {
+            if (user.getUserName() != null) {
+                nameTextView.setText(user.getUserName());
+            }
+
+            if (user.getEmail() != null) {
+                emailTextView.setText(user.getEmail());
+            }
+
+            if (user.getPhoneNumber() != null) {
+                phoneTextView.setText(user.getPhoneNumber());
+            }
+
+        });
+
+        // update the user profile based on typed info
+        view.findViewById(R.id.save_button).setOnClickListener(v -> {
+            // update first name
+            EditText userFirstNameInput = view.findViewById(R.id.edit_first_name);
+            String firstName = userFirstNameInput.getText().toString();
+
+            // display information saved message
+            Toast myToast = Toast.makeText(getActivity(), "Information Saved!",
+                    Toast.LENGTH_SHORT);
+            myToast.show();
+        });
+    }
 }
