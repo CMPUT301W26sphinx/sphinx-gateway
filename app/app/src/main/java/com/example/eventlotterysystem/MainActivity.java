6b6f1f4d255a8f0bdc14da1d1415a016037ec8cf
NotDeoxide <bryanjonathan87293@gmail.com>
2026-03-19 01:08:32 -0600

    Added Basic Organizer Fragment

--- app/app/src/main/java/com/example/eventlotterysystem/MainActivity.java
+++ app/app/src/main/java/com/example/eventlotterysystem/MainActivity.java
@@ -14,6 +14,7 @@ import androidx.core.view.WindowInsetsCompat;
 import androidx.fragment.app.Fragment;
 
 import com.example.eventlotterysystem.UI.fragments.EventListFragment;
+import com.example.eventlotterysystem.UI.fragments.OrganizerFragment;
 import com.example.eventlotterysystem.UI.fragments.ProfileFragment;
 import com.example.eventlotterysystem.UI.fragments.QRCodeFragment;
 import com.example.eventlotterysystem.database.ProfileManager;
@@ -51,6 +52,7 @@ public class MainActivity extends AppCompatActivity {
         EventListFragment eventListFragment = new EventListFragment();
         QRCodeFragment qrCodeFragment = new QRCodeFragment();
         ProfileFragment profileFragment = new ProfileFragment();
+        OrganizerFragment organizerFragment = new OrganizerFragment();
 
         // Set default fragment (Events)
         setCurrentFragment(eventListFragment);
@@ -64,6 +66,8 @@ public class MainActivity extends AppCompatActivity {
                 setCurrentFragment(profileFragment);
             } else if (id == R.id.QRScan) {
                 setCurrentFragment(qrCodeFragment);
+            } else if (id == R.id.organizer) {
+                setCurrentFragment(organizerFragment);
             }
             return true;
         });
