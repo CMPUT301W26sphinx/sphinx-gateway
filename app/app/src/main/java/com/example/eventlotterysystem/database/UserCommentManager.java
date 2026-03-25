4e3eb42019c9207da49c71209bc55e9c6a8d182e
nzapisocki <nzapisoc@ualberta.ca>
2026-03-24 20:04:58 -0600

    Implemented Add Comment
    Entrant can now add a comment to the event database.

--- app/app/src/main/java/com/example/eventlotterysystem/database/UserCommentManager.java
+++ app/app/src/main/java/com/example/eventlotterysystem/database/UserCommentManager.java
@@ -4,35 +4,47 @@ import com.example.eventlotterysystem.model.UserComment;
 import com.google.firebase.firestore.CollectionReference;
 import com.google.firebase.firestore.FieldValue;
 import com.google.firebase.firestore.FirebaseFirestore;
+import com.google.firebase.firestore.auth.User;
 
 import java.util.HashMap;
 import java.util.Map;
 
+/**
+ * This class manages stored event comments
+ */
 public class UserCommentManager {
-    /**
-     * This class manages stored event comments
-     */
 
     private FirebaseFirestore db;
     private CollectionReference eventRef;
 
-    // TODO: make this singleton
-    public UserCommentManager() {
+    private UserCommentManager() {
         db = FirebaseFirestore.getInstance();
         eventRef = db.collection("events");
     }
 
-    // TODO: add comment to an event
-    public void addCommentToEvent(String eventID, UserComment comment){
+    private static class Holder {
+        private static final UserCommentManager instance = new UserCommentManager();
+    }
+
+    public static UserCommentManager getInstance() {
+        return Holder.instance;
+    }
+
+    /**
+     * Adds the comment to the comments subcollection of an event
+     * @param eventID
+     * @param comment
+     */
+    public void addCommentToEvent(String eventID, String comment) {
         ProfileManager manager = ProfileManager.getInstance();
         // get userID
         String uid = manager.getUserID();
         // get the user
         manager.getUserProfile(user -> {
-           String firstName = user.getFirstName();
+            String firstName = user.getFirstName();
             // set fields
             Map<String, Object> data = new HashMap<>();
-            data.put("text", comment.getText());
+            data.put("text", comment);
             data.put("userID", uid);
             data.put("userName", firstName);
             data.put("timestamp", FieldValue.serverTimestamp());
