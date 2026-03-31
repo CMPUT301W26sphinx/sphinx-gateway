a2e51bb3bdbb15caa83cb1cae81a0a506fe28548
AsunaIsYip <156797289+AsunaIsYip@users.noreply.github.com>
2026-03-31 10:44:47 -0600

    Change event privacy check to exclude private events

--- app/app/src/main/java/com/example/eventlotterysystem/UI/fragments/EventListFragment.java
+++ app/app/src/main/java/com/example/eventlotterysystem/UI/fragments/EventListFragment.java
@@ -121,7 +121,7 @@ public class EventListFragment extends Fragment {
                 allEvents.clear();
                 allCategories.clear();
                 for (Event event : events) {
-                    if (event.getPrivacy() == "public"){
+                    if (event.getPrivacy() != "private"){
                         allEvents.add(event);
                         if (event.getCategory() != null && !event.getCategory().isEmpty()) {
                             allCategories.add(event.getCategory());
